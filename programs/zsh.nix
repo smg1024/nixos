ebfb4a481b2d648dbc1bac914906bbcd89fe345d
Poby <87608318+smg1024@users.noreply.github.com>
2026-03-04 13:53:49 +0900

    shell alias with hostname and whoami env variables

--- programs/zsh.nix
+++ programs/zsh.nix
@@ -13,9 +13,9 @@
 
     shellAliases = {
       poby = "echo my name is poby";
-      nrs = "sudo nixos-rebuild switch --flake ~/nixos#kmeat-nixos";
-      nrt = "sudo nixos-rebuild test --flake ~/nixos#kmeat-nixos";
-      hms = "home-manager switch -b backup --flake ~/nixos#poby";
+      nrs = "sudo nixos-rebuild switch --flake ~/nixos#$(hostname)";
+      nrt = "sudo nixos-rebuild test --flake ~/nixos#$(hostname)";
+      hms = "home-manager switch -b backup --flake ~/nixos#$(whoami)";
     };
 
     sessionVariables = {
